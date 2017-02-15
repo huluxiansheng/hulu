ba6bb20db3a7d3bce88dd5666db80918258a0d46
Mr HuLu <2407414436@qq.com>
2017-02-15 16:01:06 +0800

    test

---
+++ src/com/myLife/core/listener/ServletContextListener.java
@@ -0,0 +1,26 @@
+package com.myLife.core.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.SessionCookieConfig;
+
+public class ServletContextListener implements javax.servlet.ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext sc = sce.getServletContext();
+		SessionCookieConfig scc = sc.getSessionCookieConfig();
+		scc.setPath("/");
+		scc.setName("MYLIFEID");
+		scc.setSecure(false);
+		
+	}
+
+
+}
